3f55361a19a9cec3e013f95944320e9f1217d781
MelvinManni <melvinnnamani@gmail.com>
2020-04-14 04:02:18 +0100

    attributes set up

--- src/input/inputUi.jsx
+++ src/input/inputUi.jsx
@@ -230,8 +230,9 @@ export default function InputUi() {
                 </div>
 
                 <div className='periodType'>
-                  <label htmlFor='periodType'>Period Type</label>
+                  <label htmlFor='periodType' data-period-type >Period Type</label>
                   <select
+                    aria-label='data-period-type'
                     name='data-period-type'
                     form='estimatorForm'
                     onChange={estimateChange}
@@ -239,9 +240,15 @@ export default function InputUi() {
                     data-period-type
                   >
                     <option disabled>Select Period Type</option>
-                    <option data-period-type value='days'>Days</option>
-                    <option data-period-type value='weeks'>Weeks</option>
-                    <option  data-period-type value='months'>Months</option>
+                    <option value='days'>
+                      Days
+                    </option>
+                    <option value='weeks'>
+                      Weeks
+                    </option>
+                    <option value='months'>
+                      Months
+                    </option>
                   </select>
                 </div>
 
@@ -325,7 +332,25 @@ export default function InputUi() {
               document.getElementById('output').style.display = 'none';
             }}
           >
-            <i className='far fa-times-circle'></i>
+            <div className="close">
+              <i className='circle'>
+                <svg
+                  aria-hidden='true'
+                  focusable='false'
+                  data-prefix='far'
+                  data-icon='times-circle circle'
+                  class='svg-inline--fa fa-times-circle fa-w-16'
+                  role='img'
+                  xmlns='http://www.w3.org/2000/svg'
+                  viewBox='0 0 512 512'
+                >
+                  <path
+                    fill='currentColor'
+                    d='M256 8C119 8 8 119 8 256s111 248 248 248 248-111 248-248S393 8 256 8zm0 448c-110.5 0-200-89.5-200-200S145.5 56 256 56s200 89.5 200 200-89.5 200-200 200zm101.8-262.2L295.6 256l62.2 62.2c4.7 4.7 4.7 12.3 0 17l-22.6 22.6c-4.7 4.7-12.3 4.7-17 0L256 295.6l-62.2 62.2c-4.7 4.7-12.3 4.7-17 0l-22.6-22.6c-4.7-4.7-4.7-12.3 0-17l62.2-62.2-62.2-62.2c-4.7-4.7-4.7-12.3 0-17l22.6-22.6c4.7-4.7 12.3-4.7 17 0l62.2 62.2 62.2-62.2c4.7-4.7 12.3-4.7 17 0l22.6 22.6c4.7 4.7 4.7 12.3 0 17z'
+                  ></path>
+                </svg>
+              </i>
+            </div>
           </button>
           <div className='output-card'>
             <div className='output_title'>
@@ -358,8 +383,6 @@ export default function InputUi() {
           </div>
         </div>
       </div>
-
-      <input className='hide' type="text" data-period-type/>
     </div>
   );
 }
